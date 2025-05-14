250221931d80751ea331620114cfbd4271bb6620
Natyzinhaa <natynog887@gmail.com>
2025-05-13 21:33:16 -0300

    Validar do 1° DV

---
+++ cpf.js
@@ -0,0 +1,20 @@
+function validarCPF(cpf) {
+    cpf = cpf.replace(/[^\d]+/g, "");
+
+    if(cpf.lengt !== 11 || /^(\d)\1+$/.test(cpf)) {
+        return false;
+    }
+
+    let soma = 0;
+    let resto;
+
+    // Validar do 1° DV
+    for(let i = 1; i <= 9; i++) {
+        soma += parseInt(cpf.substring(i-1, i)) * (11 - i);
+    }
+    resto = (soma * 10) % 11;
+
+    if(resto === 10 || resto === 11) {
+        resto = 0;
+    }
+}
